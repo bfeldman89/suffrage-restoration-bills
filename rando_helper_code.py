0fa89b55072784938dc240961cd9cd93f2fae92d
Blake Feldman <26087892+bfeldman89@users.noreply.github.com>
2025-08-08 00:46:20 -0500

    Update rando_helper_code.py

--- rando_helper_code.py
+++ rando_helper_code.py
@@ -11,6 +11,7 @@ dc = DocumentCloud(os.environ['MUCKROCK_USERNAME'], os.environ['MUCKROCK_PW'])
 
 def airtab_to_dc_and_back():
     records = airtab.all(view='needs dc', fields='msleg_pdf_url', max_records=100)
+    print(f"{len(records)} records have PDFs that need to be uploaded to DC...")
     for record in records:
         this_dict = {}
         pdf_url = record['fields']['msleg_pdf_url']
@@ -18,12 +19,12 @@ def airtab_to_dc_and_back():
             obj = dc.documents.upload(pdf_url, access='public')
         except requests.exceptions.ReadTimeout:
             time.sleep(5)
-            print('sleeping bc readtimeout')
+            # print('sleeping bc readtimeout')
             continue
         obj = dc.documents.get(obj.id)
         while obj.status != 'success':
             time.sleep(5)
-            print('sleeping bc status is not "success"')
+            # print('sleeping bc status is not "success"')
             obj = dc.documents.get(obj.id)
         this_dict["dc_id"] = str(obj.id)
         print(f"successfully uploaded {obj.title}. . .")
@@ -33,4 +34,10 @@ def airtab_to_dc_and_back():
         this_dict["bill_text"] = obj.full_text
         airtab.update(record["id"], this_dict, typecast=True)
 
+def main():
+    print('the rando helper is helping!')
+    airtab_to_dc_and_back()
 
+
+if __name__ == "__main__":
+    main()
